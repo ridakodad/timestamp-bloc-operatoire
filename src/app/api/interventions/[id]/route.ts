86fa45d416cc2e03642c65c51201ab5f9cd0d54a
Rida Akodad <rida.akodad@gmail.com>
2026-04-29 17:29:39 +0100

    Fix: Force dynamic rendering for ID route

--- src/app/api/interventions/[id]/route.ts
+++ src/app/api/interventions/[id]/route.ts
@@ -1,6 +1,8 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse, NextRequest } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function PATCH(
   req: NextRequest,
   { params }: { params: Promise<{ id: string }> }
